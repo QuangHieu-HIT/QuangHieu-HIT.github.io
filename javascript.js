f547668f5b6e3e4e3254f963cc30250779555e4d
QuangHieu-HIT <62168763+QuangHieu-HIT@users.noreply.github.com>
2020-03-18 15:19:43 +0700

    Delete javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-let thumbnail = document.querySelectorAll('.thumbnail .pic img');
-let center = document.querySelector('.center .pic img')
-
-thumbnail.forEach((item) => {
-    item.addEventListener('click', (ev) => {
-        center.src = ev.target.src
-    })
-})
